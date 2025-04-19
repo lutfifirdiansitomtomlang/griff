4bd70528419af5639e2cb7bed11e4d75b15f9d42
tmdlfs <tmdlfs@gmail.com>
2025-04-19 13:57:37 +1000

    AB

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import ABTestingCalculator from './ABTestingCalculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <ABTestingCalculator />
     </div>
   );
 }
