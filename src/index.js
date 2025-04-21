006aed4f9ebd46c68cb8c8da8e71b0daa089773a
tmdlfs <tmdlfs@gmail.com>
2025-04-21 14:16:29 +1000

    Updated folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './App.css';
+import ABTestingCalculator from './ABTestingCalculator.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <ABTestingCalculator />
+    </div>
+  );
+}
+
+export default App;
